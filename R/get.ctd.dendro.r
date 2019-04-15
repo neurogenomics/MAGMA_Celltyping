29b12e7c6e8382d58b2b4fd42b37f750a7a088f5
zj-liu <zijing.liu@outloo.com>
2019-04-15 12:19:22 +0100

    Changing  to  in get.ctd.dendro

--- R/get.ctd.dendro.r
+++ R/get.ctd.dendro.r
@@ -14,7 +14,7 @@ get.ctd.dendro <- function(ctd,annotLevel){
   library(ggdendro)
   library(gridExtra)
   # Prepare dendrogram
-  binned_file_dist <- dist(t(ctd[[annotLevel]]$quantiles)) # euclidean distances between the rows
+  binned_file_dist <- dist(t(ctd[[annotLevel]]$specificity_quantiles)) # euclidean distances between the rows
   binned_file_dist_hclust <- hclust(binned_file_dist)
   ddata <- ggdendro::dendro_data(binned_file_dist_hclust, type="rectangle")
   ordered_cells <- as.character(ddata$labels$label)
