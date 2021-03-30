1b80332fbba2a2fe11d5fd99f7ca7d6797ca320a
Alan Murphy <62257358+Al-Murphy@users.noreply.github.com>
2021-03-30 17:10:04 +0100

    Issue regrading small sumstats file, no overlap

--- R/get_genomebuild_for_sumstats.r
+++ R/get_genomebuild_for_sumstats.r
@@ -12,13 +12,22 @@
 #' @export
 #' @importFrom utils read.table
 get_genomebuild_for_sumstats <- function(path){
-    # Get first 20 lines of sumstats
-    topLines = utils::read.table(path,nrows=30000,header = TRUE,stringsAsFactors = FALSE)
-    topSNPS  = topLines$SNP
-    topLOCs  = sprintf("%s-%s-%s",topLines$SNP,topLines$CHR,topLines$BP)
-    SNP_LOC_DATA = load_snp_loc_data()
-    sub_SNP_LOC_DATA = SNP_LOC_DATA[sample(1:dim(SNP_LOC_DATA)[1],100000),]
-    topSNP_locs = sub_SNP_LOC_DATA[sub_SNP_LOC_DATA$SNP %in% topLines$SNP,]
-    topSNP_locs$locs = sprintf("%s-%s-%s",topSNP_locs$SNP,topSNP_locs$CHR,topSNP_locs$BP)
-    return(names(sort(table(topSNP_locs[topSNP_locs$locs %in% topLOCs,]$Build),decreasing = TRUE)[1]))
+  # Get first 20 lines of sumstats
+  topLines = 
+    utils::read.table(path,nrows=30000,header = TRUE,stringsAsFactors = FALSE)
+  topSNPS  = topLines$SNP
+  topLOCs  = sprintf("%s-%s-%s",topLines$SNP,topLines$CHR,topLines$BP)
+  SNP_LOC_DATA = load_snp_loc_data()
+  sub_SNP_LOC_DATA = SNP_LOC_DATA[sample(1:dim(SNP_LOC_DATA)[1],100000),]
+  topSNP_locs = sub_SNP_LOC_DATA[sub_SNP_LOC_DATA$SNP %in% topLines$SNP,]
+  #If no matches found in the sampled reference dataset use full
+  if(nrow(topSNP_locs)==0)
+    topSNP_locs = SNP_LOC_DATA[SNP_LOC_DATA$SNP %in% topLines$SNP,]
+  #if still none found there is an issue
+  if(nrow(topSNP_locs)==0)
+    stop(paste0("No SNPs from the inputted summary statistics file match ",
+                  "reference data. Please check SNP ID's are correct."))
+  topSNP_locs$locs = 
+    sprintf("%s-%s-%s",topSNP_locs$SNP,topSNP_locs$CHR,topSNP_locs$BP)
+  return(names(sort(table(topSNP_locs[topSNP_locs$locs %in% topLOCs,]$Build),decreasing = TRUE)[1]))
 }
