f3663f971a30decbb49c874ad7f8a4a8878efebb
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2021-12-20 17:23:02 +0000

    Fix wrong magma version install. \nocache

--- tests/testthat/test-magma_install.R
+++ tests/testthat/test-magma_install.R
@@ -31,7 +31,7 @@ test_that("magma_install/uninstall works", {
       testthat::expect_true(is.character(uninstalled))
       testthat::expect_length(uninstalled, 2)
       testthat::expect_false(all(file.exists(uninstalled)))
-    } else {
+    } else { 
       testthat::expect_null(NULL)
     }
 })
