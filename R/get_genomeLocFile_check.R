ce9cb5ab5cd12dc29672686ebd54eccf1ee3cf6e
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2022-04-07 11:01:46 +0100

    Add detailed fix instructions for get_genomeLocFile

---
+++ R/get_genomeLocFile_check.R
@@ -0,0 +1,19 @@
+get_genomeLocFile_check <- function(tmp){
+    l <- readLines(tmp)
+    if(any(grepl("Bad credentials",l, ignore.case = TRUE))){
+        file.remove(tmp)
+        stp <- paste(
+            "genomeLocFile download failed due to bad GitHub credentials.",
+            "Please add a GitHub Personal Access Token (PAT)",
+            "to your ~/.Renviron file and restart R",
+            "before retrying this function.\ne.g.:\n\n",
+            "   GITHUB_TOKEN=<your_PAT_here>",
+            "\n\nIf you do not yet have a GitHub PAT,",
+            "please follow instructions here:\n\n   ",
+            paste("https://docs.github.com/en/authentication",
+            "keeping-your-account-and-data-secure",
+            "creating-a-personal-access-token",sep="/")
+            )
+        stop(stp)
+    }
+}
